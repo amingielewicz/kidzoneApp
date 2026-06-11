ae6465c8bc29bb32e85fbaef503131e3a538abaa
Adam <adammingielewicz@gmail.com>
2026-06-12 01:47:18 +0200

    fix: withFirestoreTimeout compile error (remove inline/crossinline)

--- app/src/main/java/com/kidzone/utils/AppConfig.kt
+++ app/src/main/java/com/kidzone/utils/AppConfig.kt
@@ -75,7 +75,6 @@ object AppConfig {
      * przy każdej istotnej zmianie tekstu w [com.kidzone.presentation.profile.PrivacyPolicyDialog].
      */
     const val PRIVACY_POLICY_EFFECTIVE_DATE: String = "29.05.2026"
-}
 
 /**
  * Inline helper eliminating repetitive timeout + error-wrapping boilerplate
