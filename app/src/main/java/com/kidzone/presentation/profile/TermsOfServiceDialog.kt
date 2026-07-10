77c6f78442c58063a3801e6ac86e912c3605324a
Adam <adammingielewicz@gmail.com>
2026-07-11 01:09:01 +0200

    Napraw import linkow w regulaminie

--- app/src/main/java/com/kidzone/presentation/profile/TermsOfServiceDialog.kt
+++ app/src/main/java/com/kidzone/presentation/profile/TermsOfServiceDialog.kt
@@ -21,6 +21,7 @@ import androidx.compose.ui.text.TextLinkStyles
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withLink
 import androidx.compose.ui.unit.dp
 import com.kidzone.R
 
